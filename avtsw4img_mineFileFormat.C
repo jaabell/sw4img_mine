dc05ae4b8634c66824fa076b080aca892098346e
Jose Abell <jaabell@ucdavis.edu>
2015-11-14 23:20:16 -0800

    added the almighty globVector function!!!

--- avtsw4img_mineFileFormat.C
+++ avtsw4img_mineFileFormat.C
@@ -79,6 +79,25 @@ using namespace std;
 #define LSEEK ::lseek
 #endif
 
+#include <glob.h>
+#include <vector>
+using std::vector;
+
+vector<string> globVector(const string& pattern)
+{
+    glob_t glob_result;
+    glob(pattern.c_str(), GLOB_TILDE, NULL, &glob_result);
+    vector<string> files;
+    for (unsigned int i = 0; i < glob_result.gl_pathc; ++i)
+    {
+        files.push_back(string(glob_result.gl_pathv[i]));
+    }
+    globfree(&glob_result);
+    return files;
+}
+
+
+
 // ****************************************************************************
 //  Method: avtsw4img_mineFileFormat constructor
 //
